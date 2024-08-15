4fe6711c4830aad9a8b8497120b00efc5a11c7cb
Jakub Kunikowski <j.kunikowski.165@studms.ug.edu.pl>
2024-08-15 13:13:34 +0200

    some scraped data for personal use

---
+++ src/main.py
@@ -0,0 +1,35 @@
+import requests
+from bs4 import BeautifulSoup
+import pprint
+
+
+def sort_stories_by_votes(hnlist):
+    return sorted(hnlist, key=lambda k: k["votes"], reverse=True)
+
+
+def create_custom_hn(links, subtext):
+    hn = []
+    for inx, item in enumerate(links):
+        title = item.getText()
+        href = item.get("href", None)
+        vote = subtext[inx].select('.score')
+        if len(vote):
+            points = int(vote[0].getText().replace(' points', ''))
+            if points > 99:
+                hn.append({"title": title, "link": href, "votes": points})
+    return hn
+
+
+def view_few_pages(numOfPages):
+    result = []
+    for i in range(1, numOfPages + 1):
+        res = requests.get(f"https://news.ycombinator.com/news?p={i}")
+        soup = BeautifulSoup(res.text, 'html.parser')
+        links = soup.select('.titleline')
+        subtext = soup.select('.subtext')
+        result.extend(create_custom_hn(links, subtext))
+
+    return sort_stories_by_votes(result)
+
+
+pprint.pprint(view_few_pages(3))
